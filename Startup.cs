3d01a5630d44eb79f0a29ae32f9a8e43878327f0
Pavle Stepanić <pavle.stepanic@noubis.com>
2020-10-14 10:07:14 +0200

    WebSocketServerCommit

--- Startup.cs
+++ Startup.cs
@@ -22,15 +22,37 @@ namespace WebSocketServer
 
             app.Use(async (context, next) =>
             {
+                WriteRequestParam(context);
                 if(context.WebSockets.IsWebSocketRequest)
                 {
                     WebSocket webSocket = await context.WebSockets.AcceptWebSocketAsync();
                     Console.WriteLine("WebSocket Connected");
                 }
                 else{
+                    Console.WriteLine("Hello from the 2nd request delegate.");
                     await next();
                 }
             });
+
+            app.Run(async context =>
+            {
+                Console.WriteLine("Hello from the 3rd request delegate.");
+                await context.Response.WriteAsync("Hello from the 3rd request delegate.");
+            });
+        }
+
+        public void WriteRequestParam(HttpContext context)
+        {
+            Console.WriteLine("Request Method: " + context.Request.Method);
+            Console.WriteLine("Request Protocol: " + context.Request.Protocol);
+
+            if(context.Request.Headers != null)
+            {
+                foreach(var h in context.Request.Headers)
+                {
+                    Console.WriteLine("--> " + h.Key + " : " +h.Value);
+                }
+            }
         }
     }
 }
